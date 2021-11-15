81a4586905831cf838053629a5ccbffa91a7e5c6
roblebob <robert.koppisch@gmail.com>
2021-11-15 18:15:18 +0100

    up to 3.6

--- 3.05-Exercise-ConfigureBuildTypes/app/src/main/java/com/udacity/gradle/buildtypes/MainActivity.java
+++ 3.05-Exercise-ConfigureBuildTypes/app/src/main/java/com/udacity/gradle/buildtypes/MainActivity.java
@@ -1,8 +1,8 @@
 package com.udacity.gradle.buildtypes;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
