ad8b89adf24df8c70bb99db899a1d64f366cea6a
roblebob <robert.koppisch@gmail.com>
2021-11-17 13:12:50 +0100

    finished all 3.x

--- 3.09-Demo-Flavor-SpecificDependencies/app/src/free/java/com/udacity/gradle/adsupported/MainActivityFragment.java
+++ 3.09-Demo-Flavor-SpecificDependencies/app/src/free/java/com/udacity/gradle/adsupported/MainActivityFragment.java
@@ -1,13 +1,18 @@
 package com.udacity.gradle.adsupported;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -23,13 +28,27 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
+
+
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+
+        List<String> testDevices = new ArrayList<>();
+        testDevices.add( AdRequest.DEVICE_ID_EMULATOR);
+
+        RequestConfiguration requestConfiguration
+                = new RequestConfiguration.Builder()
+                    .setTestDeviceIds(testDevices)
+                    .build();
+
+        MobileAds.setRequestConfiguration(requestConfiguration);
+
         AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                //.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
+
         mAdView.loadAd(adRequest);
         return root;
 
